e6708deca8cb2a6a45eddd6b96696f825671c61c
Dominic Cooney <dominic.cooney@gmail.com>
2012-06-29 18:44:39 +0900

    Be more aggressive about removing existing configuration files.

--- .profile
+++ .profile
@@ -1,9 +1,17 @@
-export PATH=/opt/local/bin:/opt/local/sbin:$PATH
+if [[ $(uname) = 'Darwin' ]]; then
+  export PATH=/opt/local/bin:/opt/local/sbin:$PATH
+  export JAVA_HOME=$(/usr/libexec/java_home)
+  export PATH=~/android-sdk-macosx/tools:~/android-sdk-macosx/platform-tools:$PATH
+  alias emacs=emacs-snapshot
+fi
+
 export EDITOR=emacsclient
 export GIT_EDITOR=emacsclient
 export SVN_LOG_EDITOR=emacsclient
 export VISUAL=emacsclient
-export JAVA_HOME=$(/usr/libexec/java_home)
-export PATH=~/android-sdk-macosx/tools:~/android-sdk-macosx/platform-tools:$PATH
 
-alias emacs=emacs-snapshot
+if [ -n "$BASH_VERSION" ]; then
+  if [ -f "$HOME/.bashrc" ]; then
+    . "$HOME/.bashrc"
+  fi
+fi
--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+set -e
+
 EMACS=emacs
 
 pushd $(dirname $0) > /dev/null
   REPO_DIR=$(pwd)
 popd > /dev/null
 
-if [ $(uname) = 'Darwin' ]; then
+if [[ $(uname) = 'Darwin' ]]; then
   EMACS=emacs-snapshot
 
-  if [ -z $(which port) ]; then
+  if [[ -z $(which port) ]]; then
       echo Install MacPorts first
       exit 1
   fi
@@ -19,39 +21,39 @@ if [ $(uname) = 'Darwin' ]; then
 
   for port in git-core screen emacs-snapshot irssi R
   do
-    if [ -z $(port list installed and $port) ]; then
+    if [[ -z $(port list installed and $port) ]]; then
       sudo port install $port
     fi
   done
 
   sudo port install git-core +svn
-elif [ $(uname) = 'Linux' ]; then
+elif [[ $(uname) = 'Linux' ]]; then
   for package in subversion screen emacs emacs-goodies-el git-core xsel curl xmonad
   do
-    if [ -z $(dpkg -s $package | grep 'Status: install ok installed') ]; then
+    if [[ -z $(dpkg -s $package | grep 'Status: install ok installed') ]]; then
       sudo apt-get install $package
     fi
   done
 
   for editor_spec in EDITOR GIT_EDITOR SVN_LOG_EDITOR VISUAL
   do
-    if [ -z $(grep $editor_spec= ~/.bashrc) ]; then
+    if [[ -z $(grep $editor_spec= ~/.bashrc) ]]; then
       echo $editor_spec=emacsclient >> ~/.bashrc
     fi
   done
 
-  if [ -z $(grep TERM=xterm-256color ~/.bashrc) ]; then
+  if [[ -z $(grep TERM=xterm-256color ~/.bashrc) ]]; then
     echo TERM=xterm-256color >> ~/.bashrc
   fi
 
   # Set up aliases to act like pbcopy and pbpaste on Mac OS X
-  if [ -z $(grep pbcopy ~/.bashrc) ]; then
+  if [[ -z $(grep pbcopy ~/.bashrc) ]]; then
     echo alias pbcopy=\'xsel --clipboard --input\' >> ~/.bashrc
     echo alias pbpaste=\'xsel --clipboard --output\' >> ~/.bashrc
   fi
 fi
 
-if [ -z $(which git) ]; then
+if [[ -z $(which git) ]]; then
   echo Install git
   exit 1
 fi
@@ -60,7 +62,7 @@ fi
 # with the same name
 git config --global push.default tracking
 
-if [ ! -d ~/webkit-tools ]; then
+if [[ ! -d ~/webkit-tools/.git ]]; then
   # Fetch webkit-tools
   pushd ~ > /dev/null
   git clone git@github.com:coonsta/webkit-tools.git
@@ -75,7 +77,7 @@ fi
 
 mkdir -p ~/site-lisp
 pushd ~/site-lisp > /dev/null
-  if [ ! -f color-moccur.el ]; then
+  if [[ ! -f color-moccur.el ]]; then
     curl -O http://www.bookshelf.jp/elc/color-moccur.el || exit 1
     curl -O http://www.emacswiki.org/emacs/download/moccur-edit.el || exit 1
     $EMACS -Q --batch --eval '(byte-compile-file "color-moccur.el")
@@ -83,11 +85,11 @@ pushd ~/site-lisp > /dev/null
   fi
 
   # Clean up old js2-mode
-  if [ -d js2-mode-read-only ]; then
+  if [[ -d js2-mode-read-only ]]; then
     rm -rf js2-mode-read-only
   fi
 
-  if [ ! -d js2-mode ]; then
+  if [[ ! -d js2-mode ]]; then
     git clone https://github.com/dgutov/js2-mode.git
     pushd js2-mode > /dev/null
       git checkout -b emacs24 origin/emacs24
@@ -99,7 +101,7 @@ pushd ~/site-lisp > /dev/null
     $EMACS --batch -f batch-byte-compile js2-mode.el
   popd > /dev/null
 
-  if [ ! -d zenburn-emacs ]; then
+  if [[ ! -d zenburn-emacs ]]; then
     git clone git://github.com/bbatsov/zenburn-emacs.git
   else
     pushd zenburn-emacs > /dev/null
@@ -111,10 +113,15 @@ popd > /dev/null
 
 for config_file in .emacs .gdbinit .screenrc .profile
 do
+  if [[ -h ~/$config_file ]]; then
+    rm ~/$config_file
+  else
+    rm -if $config_file
+  fi
   ln -s $REPO_DIR/$config_file ~/$config_file
 done
 
-if [ $(uname) = 'Linux' ]; then
+if [[ $(uname) = 'Linux' ]]; then
   for config_file in .Xmodmap .xmonad
   do
     ln -s $REPO_DIR/$config_file ~/
@@ -125,4 +132,3 @@ if [ $(uname) = 'Linux' ]; then
   sudo cp $REPO_DIR/xmonad.session /usr/share/gnome-session/sessions
   sudo cp $REPO_DIR/xmonad-unity-session.desktop /usr/share/xsessions
 fi
-
